e82ea9e91430b07442d86da1ca1857dd6b4945b2
Sitaratoons <mrajayh393798@gmail.com>
2025-02-01 19:26:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from os import environ
 
-API_HASH = environ.get("API_HASH", "")
-API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "29bf447b916a795191046a91317869fb")
+API_ID = int(environ.get("API_ID", "28918271"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_OWNER = int(environ.get("BOT_OWNER", ""))
 BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
