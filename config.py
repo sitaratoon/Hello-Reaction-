a75e21de09326a54127154477a6683e56c7f9b1c
Sitaratoons <mrajayh393798@gmail.com>
2025-02-01 19:29:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ API_HASH = environ.get("API_HASH", "29bf447b916a795191046a91317869fb")
 API_ID = int(environ.get("API_ID", "28918271"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_OWNER = int(environ.get("BOT_OWNER", ""))
-BOT_USERNAME = environ.get("BOT_USERNAME", "QuickReactRobot")
+BOT_USERNAME = environ.get("BOT_USERNAME", "Autoreaction02bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
-DATABASE_URL = environ.get("DATABASE_URL", "")
+DATABASE_URL = environ.get("DATABASE_URL", "mongodb+srv://sitaratoons:sitaratoons@cluster0.98nq3.mongodb.net/?retryWrites=true&w=majority")
 
 # Define default emojis list
 EMOJIS = [
