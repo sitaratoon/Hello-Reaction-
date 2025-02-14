c3134660ff8224f9ec3ed7c034f5a0887b10e48c
sitaratoon <ajay393798@gmail.com>
2025-02-14 11:50:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 API_HASH = environ.get("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
 API_ID = int(environ.get("API_ID", "18946488"))
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_OWNER = int(environ.get("BOT_OWNER", "7475464684"))
+BOT_OWNER = int(environ.get("BOT_OWNER", "7661512835"))
 BOT_USERNAME = environ.get("BOT_USERNAME", "Autoreaction08bot")
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002425566477"))
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", "-1002355394644"))
